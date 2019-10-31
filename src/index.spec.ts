70c29678f50ceae13c70925ffa2818783ea36ae0
martinb <martinbraun123@aol.com>
2019-10-31 15:43:17 +0100

    add unit test for empty list edge case

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,13 +1,13 @@
 import { parse, Control, ListChunk } from "../lib/index"
 import * as _ from 'lodash';
 
-describe('test lazy loading list', () => {
-    const tenPages: Control<number, string> = {
+function pages(pageCount: number) {
+    const ret: Control<number, string> = {
         hasNext(page: number | null) {
             if(page != null) {
-                return page < 9;
+                return page < (pageCount - 1);
             } else {
-                return true;
+                return pageCount > 0;
             }
         },
         next(page: number | null) {
@@ -21,6 +21,11 @@ describe('test lazy loading list', () => {
             return Promise.resolve(_.repeat(`${page}`, 10).split(''));
         }
     };
+    return ret;
+}
+
+describe('test lazy loading list', () => {
+    const tenPages = pages(10);
 
     it('test 10 pages with next', () => {        
         return parse(tenPages).then(async listChunk => {
@@ -52,7 +57,23 @@ describe('test lazy loading list', () => {
                 expect(elem).toEqual(expected[idx]);
                 idx = idx + 1;
             }
+            expect(idx).toEqual(100);
         })
-    })
+    });
 
+});
+
+describe('edge cases', () => {
+    it('empty list', () => {
+        return parse(pages(0)).then(async listChunk => {
+            expect(listChunk.elements).toEqual([]);
+            expect(listChunk.next).toEqual(null);
+
+            let once = false;
+            for await(const elem of listChunk.untilEnd()) {
+                once = true;
+            }
+            expect(once).toBeFalsy();
+        });
+    });
 })
